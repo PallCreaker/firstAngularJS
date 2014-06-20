8dda8190786f6e23ccd5c83afcfaf4f6a925a813
iMac for local <pallcreaker@gmail.com>
2014-06-20 22:06:03 +0900

    button click update

--- js/myscript.js
+++ js/myscript.js
@@ -3,5 +3,17 @@ var mainCtrl = function($scope){
         {"name" : "fuji", "score" : 23.23 },
         {"name" : "fuji", "score" : 231434.23 },
         {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
     ];
+};
+
+var userItemCtrl = function($scope){
+    $scope.increment = function(){
+        $scope.user.score ++;
+    };
 };
