eb0d41ebcfc8fc040f50f12dcec50704b50160b8
iMac for local <pallcreaker@gmail.com>
2014-06-20 21:52:25 +0900

    ng-controller use

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+var mainCtrl = function($scope){
+    $scope.users = [
+        {"name" : "fuji", "score" : 23.23 },
+        {"name" : "fuji", "score" : 231434.23 },
+        {"name" : "fuji", "score" : 2243433.23 },
+    ];
+};
